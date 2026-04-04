3109fa145803e41718547fc1610a1b285d52400d
Luis Donaldo <luis.dbg1123@outlook.com>
2026-04-04 15:22:47 -0600

    Modify Personal Introduction Program

--- 01_syntax_structure/02_PersonalIntroduction/personal_intro.cpp
+++ 01_syntax_structure/02_PersonalIntroduction/personal_intro.cpp
@@ -1,3 +1,17 @@
+/*
+* Personal Introduction Program
+* Created by: WR1123
+* Date: 04/04/2026
+*
+* Description: This program displays personal information
+* about myself in a formatted way on the console.
+*/
+
+
+/**
+ * My Learning Goals:
+ * learn C++
+ */
 #include <iostream>
 
 // Main function - program execution starts here
