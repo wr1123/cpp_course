0a3aaab0901901791fed1239d462a763f5c560e4
Luis Donaldo <luis.dbg1123@outlook.com>
2026-04-05 10:39:41 -0600

    Add headers to work with dates and random numbers

--- 01_syntax_structure/03_ExploringProgramStructureThroughModification/multi_component.cpp
+++ 01_syntax_structure/03_ExploringProgramStructureThroughModification/multi_component.cpp
@@ -2,6 +2,8 @@
 #include <iostream>  // For input/output operations
 #include <iomanip>   // For output formatting
 #include <string>    // For string data type
+#include <random> // Para rendom numbers
+#include <chrono> // Para dates
 
 // Using the standard namespace
 using namespace std;
