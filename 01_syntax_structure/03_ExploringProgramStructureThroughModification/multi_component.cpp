82788db4afe13331008e13e9d554273626bad0af
Luis Donaldo <luis.dbg1123@outlook.com>
2026-04-05 10:37:24 -0600

    Create multi_component.cpp

---
+++ 01_syntax_structure/03_ExploringProgramStructureThroughModification/multi_component.cpp
@@ -0,0 +1,7 @@
+// Include necessary headers
+#include <iostream>  // For input/output operations
+#include <iomanip>   // For output formatting
+#include <string>    // For string data type
+
+// Using the standard namespace
+using namespace std;
